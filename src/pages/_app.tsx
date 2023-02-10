a6b7cd240c70d368ef8db71603e1e2f9abb6f8d3
masakinihirota <masakinihirota@gmail.com>
2023-02-11 03:27:43 +0900

    _app.tsx _document.tsx設定終了

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import { type AppType } from "next/app";
-
 import { api } from "../utils/api";
-
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = (props) => {
+  const { Component, pageProps } = props;
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content=" initial-scale=1, width=device-width" />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
