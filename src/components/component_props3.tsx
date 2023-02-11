32d67593659d72bcdcfa033c6edf888064b0689e
masakinihirota <masakinihirota@gmail.com>
2023-02-11 17:18:13 +0900

    入門2 まで終了 ＋リンクの整頓

---
+++ src/components/component_props3.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import {
+  Button,
+  Card,
+  Container,
+  MantineProvider,
+  Paper,
+  Slider,
+  Text,
+} from "@mantine/core";
+
+const StylesAPI3: NextPage = () => {
+  return (
+    <>
+      111
+      <Container></Container>
+    </>
+  );
+};
+
+export default StylesAPI3;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,6 @@ import { api } from "../utils/api";
 
 // Mantine
 import { Button } from "@mantine/core";
-// 自作コンポーネント
-import SomePart from "../components/somepart";
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -23,17 +21,23 @@ const Home: NextPage = () => {
       </Head>
 
       <div className="text-2xl text-orange-700">
+        tRPCのデモ <br />
         {hello.data ? hello.data.greeting : "Loading tRPC query..."}
       </div>
       <Link href="/value">
-        <Button className="text-2xl text-blue-800">
-          value(名前は適当)ページに移動
+        <Button className=" text-blue-800">
+          Link: value(名前は適当)ページに移動
         </Button>
       </Link>
-      <SomePart label="hello" name="default" />
       <Link href="/component_props">
-        <Button className="text-2xl text-blue-800">
-          component propsページに移動
+        <Button my="200" className=" text-blue-800">
+          Link: Mantine 入門1 セットアップとリンクの貼り方、テーマの概要 <br />
+          Link: Mantine 入門2 スタイルを書く
+        </Button>
+      </Link>
+      <Link href="/component_props3">
+        <Button className=" text-blue-800">
+          Link: Mantine 入門3 主要パーツ一覧
         </Button>
       </Link>
     </>
