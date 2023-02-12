6de888eaab12424af1ab28ec0fddac99607f00c3
masakinihirota <masakinihirota@gmail.com>
2023-02-12 12:32:30 +0900

    入門4のした準備完了

---
+++ src/pages/component_props4.tsx
@@ -0,0 +1,21 @@
+// Next.js
+import type { NextPage } from "next";
+import Link from "next/link";
+// Mantine
+import { Button } from "@mantine/core";
+// 自作コンポーネント
+
+const Value: NextPage = () => {
+  return (
+    <>
+      <div className="text-green-700">
+        component_propsページ
+        <Link href="/">
+          <Button className="text-purple-600">TOPに移動</Button>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default Value;
